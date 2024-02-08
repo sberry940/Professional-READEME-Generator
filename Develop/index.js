da8341dac697bc02b5b9fe562534a1545abd930b
steve <stevemberry2@gmail.com>
2024-02-08 15:07:02 -0600

    adjusts questions and created function to write readme file and initialized app

--- Develop/index.js
+++ Develop/index.js
@@ -3,35 +3,52 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const path = require('path');
 const generateMarkdown = require('./utils/generateMarkdown');
+
 // TODO: Create an array of questions for user input
 const questions = [
     {
-        type: 'input',
-        name: 'title',
-        message: 'Please name your project.'
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Describe the functionality and purpose of this project.',
-    },
-    {
-        type: 'checkbox',
-        name: 'license',
-        message: 'Please select a license for this project.',
-        choices: ['MIT', 'APACHE2.0', 'Boost1.0', 'MPL2.0', 'BSD2', 'BSD3', 'None'],
-    },
-    {
-        type: 'input',
-        name: 'require',
-        message: 'List any project dependencies here.',
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'State the languages or technologies associated with this project.',
-    },
-    {
+        type: "input",
+        name: "title",
+        message: "Please name your Project.",
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Please describe the purpose and functionality of this project.",
+      },
+      {
+        type: "input",
+        name: "screenshot",
+        message: "Please provide the relative path to the image you want to use as the screenshot."
+      },
+      {
+        type: "input",
+        name: "link",
+        message: "Please provide a URL where a user can access your deployed application."
+      },
+      {
+        type: "checkbox",
+        name: "license",
+        message: "Please select a license applicable to this project.",
+        choices: ["MIT", "APACHE2.0", "Boost1.0", "MPL2.0", "BSD2", "BSD3", "none"],
+      },
+      {
+        type: "input",
+        name: "require",
+        message: "List any project dependencies here.",
+      },
+      {
+        type: "input",
+        name: "features",
+        message: "List some cool features about this project here.",
+      },
+      {
+        type: "input",
+        name: "usage",
+        message:
+          "State the languages or technologies associated with this project.",
+      },
+      {
         type: "input",
         name: "creator",
         message: "Write your GitHub username.",
@@ -55,10 +72,17 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((responses) => {
+        console.log('Creating your Projessional README.md file...');
+        writeToFile('./dist/README.md', generateMarkdown({...responses }));
+    })
+}
 
 // Function call to initialize app
 init();
