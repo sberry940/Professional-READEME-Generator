9fb92f45845b60cd1a31084cfa2baf66b3d03ecd
steve <stevemberry2@gmail.com>
2024-02-08 15:23:13 -0600

    if statements to render a license

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -5,6 +5,23 @@ function renderLicenseBadge(liscense) {
   }
   return '';
 }
+//function that returns license link
+function renderLicenseLink(license) {
+  if(license !== 'none') {
+    return `\n* [license](#license)\n`;
+  }
+  return '';
+}
+
+//function that returns license section
+function renderLicenseBadge(license) {
+  if (license !== 'none') {
+    return `## License
+    Licensed under the ${license} license.`;
+  }
+  return '';
+}
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
