9cac5c379a2ef285dda26949a236ee011cb75f81
steve <stevemberry2@gmail.com>
2024-02-08 16:02:53 -0600

    added markdown function

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -26,7 +26,32 @@ function renderLicenseBadge(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ${renderLicenseBadge(data.license)}
+  ## Description
+  ${data.description}
+  ## Deployed Application URL
+  ${data.link}
+  ## Screenshot
+  ![alt-text](${data.screenshot})
+  ## Table of Contents
+  * [Features](#features)
+  * [Languages & Dependencies](#languagesanddependencies)
+  * [How to Use This Application](#HowtoUseThisApplication)
+  * [Contributors](#contributors)
+  * [Testing](#testing)
+  * [Questions](#questions)
+  ## Features
+  ${data.features}
+  ## Languages & Dependencies
+  ${data.require}
+  ## How to Use This Application:
+  ${data.usage}
+  ## Contributors
+  ${data.contributors}
+  ## Testing
+  ${data.test}
+  ## Questions
+  Please send your questions [here](mailto:${data.email}?subject=[GitHub]%20Dev%20Connect) or visit [github/${data.creator}](https://github.com/${data.creator}).
 `;
 }
 
